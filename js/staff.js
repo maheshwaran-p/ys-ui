8e955e78cbf942f2177585e7656604a70d402dc6
maheshwaran-p <mpmahesh1022@gmail.com>
2022-04-18 00:20:15 +0530

    graph data reversed and percentage ceil

--- js/staff.js
+++ js/staff.js
@@ -1,7 +1,7 @@
 
 
-// BASEURL = 'http://65.0.119.56:4000';
-BASEURL = 'http://localhost:4000';
+BASEURL = 'http://65.0.119.56:4000';
+// BASEURL = 'http://localhost:4000';
 async function getCourse(id) {
     console.log("ID")
     console.log(id)
