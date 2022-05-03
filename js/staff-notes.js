7e45b54cf1c2d312f6bb2d44fdfe391680d39f4f
maheshwaran-p <mpmahesh1022@gmail.com>
2022-05-03 22:14:47 +0530

    https

--- js/staff-notes.js
+++ js/staff-notes.js
@@ -1,6 +1,6 @@
 
-BASEURL = 'http://65.0.119.56:4000';
-// BASEURL = 'http://localhost:4000';
+BASEURL = 'https://65.0.119.56:4000';
+// BASEURL = 'https://localhost:4000';
 async function addnotes() {
 
     console.log(document.getElementById('title').value)
